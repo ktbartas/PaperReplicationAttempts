a9ee3de64882c871c50a5ab978094b3e254d56ce
ktbartas <70544196+ktbartas@users.noreply.github.com>
2020-10-04 23:26:34 -0700

    made reading/changing things a bit easier

--- TysonJJ_recreate3.m
+++ TysonJJ_recreate3.m
@@ -5,14 +5,21 @@ clc;close all;
 %amino acid concentration
 aa=1; %this is a constant but idk what the value is supposed to be
 
+%how many minutes do you want to graph =Tmax
+Tmax=100
+%initial conditions
+ei1= 0.001 ;
+ei2= 0 ;
+ei3= 0 ;
+ei4= 0.001 ;
+ei5= 0.0001 ;
+ei6= 0 ;
+CTinit= ei1+ei2+ei3+ei4;
+
 %define rates - given in table 2
-k1 = .015*.002/aa; %min^-1
-%^^^IF YOU CHANGE THE INITIAL VALUES, change .002 above to whatever number is
-%printed repeatedly below after you run 
+k1 = .015*CTinit/aa; %min^-1
 k2=0;%min^-1
-k3=200/.002;%min^-1
-%^^^IF YOU CHANGE THE INITIAL VALUES, change .002 above to whatever number is
-%printed repeatedly below after you run 
+k3=200/CTinit;%min^-1
 k4=180; %adjustable - will change later
 kprime4=0.018;%min^-1
 k5=0;%min^-1
@@ -29,7 +36,7 @@ eq4 =@(C2,CP,pM,M,Y,YP) pM*(kprime4 + k4*(M/(C2 + CP + pM + M))^2) - k5*M - k6*M
 eq5 =@(C2,CP,pM,M,Y,YP) k1*aa - k2*Y - k3*CP*Y   ;
 eq6 =@(C2,CP,pM,M,Y,YP) k6*M - k7*YP   ;
 
-[time, ex] = ode45(@(a,b)[eq1(b(1),b(2),b(3),b(4),b(5),b(6));eq2(b(1),b(2),b(3),b(4),b(5),b(6));eq3(b(1),b(2),b(3),b(4),b(5),b(6));eq4(b(1),b(2),b(3),b(4),b(5),b(6));eq5(b(1),b(2),b(3),b(4),b(5),b(6));eq6(b(1),b(2),b(3),b(4),b(5),b(6))], [0,100], [0.001,0,0,0.001,0.0001,0] );
+[time, ex] = ode45(@(a,b)[eq1(b(1),b(2),b(3),b(4),b(5),b(6));eq2(b(1),b(2),b(3),b(4),b(5),b(6));eq3(b(1),b(2),b(3),b(4),b(5),b(6));eq4(b(1),b(2),b(3),b(4),b(5),b(6));eq5(b(1),b(2),b(3),b(4),b(5),b(6));eq6(b(1),b(2),b(3),b(4),b(5),b(6))], [0,Tmax], [ei1,ei2,ei3,ei4,ei5,ei6] );
 %add pM, M, Y and YP together for Ytotal
 Ytot=ex(:,3)+ex(:,4)+ex(:,5)+ex(:,6);
 Ctot=ex(:,1)+ex(:,2)+ex(:,3)+ex(:,4)
